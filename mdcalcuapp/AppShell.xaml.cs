628cd16b68157a5d91b4df261579365ceb0529d8
Alex Abainza <abainza.alexandra@gmail.com>
2024-02-28 10:19:01 +0800

    Add project files.

---
+++ mdcalcuapp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mdcalcuapp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ mdcalcuapp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace mdcalcuapp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
