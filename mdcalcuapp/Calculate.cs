eabbe6ed4f4f199435baaf52ac8b4d080e443b5b
Alex Abainza <abainza.alexandra@gmail.com>
2024-03-06 01:17:29 +0800

    fixed operations ish

---
+++ mdcalcuapp/Calculate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdcalcuapp
+{
+    public static class Calculate
+    {
+        public static double DoCalculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+
+                case "+":
+                    result = num1 + num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+    }
+}
--- mdcalcuapp/MainPage.xaml.cs
+++ mdcalcuapp/MainPage.xaml.cs
@@ -5,101 +5,87 @@ namespace mdcalcuapp
 {
     public partial class MainPage : ContentPage
     {
-
+        int currentState = 1;
+        string operation;
+        double firstNum, secondNum;
 
         public MainPage()
         {
             InitializeComponent();
+            btnCanc_Clicked(this, null);
         }
 
         private void btnOperation_Clicked(object sender, EventArgs e)
         {
-            Button b=(Button)sender;
-            if(lblHistory.Text == "0")
-            {
-                lblHistory.Text = string.Format("{0} {1}", lblValue.Text, b.Text);
-            }
-            else
-            {
-                lblHistory.Text = string.Format("{0} {1} {2}", lblHistory.Text, lblValue.Text, b.Text);
-            }
+            currentState = -2;
 
-            //adds text to the previous values
-            lblValue.Text = "0";
+            Button b=(Button)sender;
+            operation = b.Text;
         }
 
         private void btnCanc_Clicked(object sender, EventArgs e)
         {
-            lblHistory.Text = "0";
-            lblValue.Text = "0";
+            firstNum = 0;
+            secondNum = 0;
+            currentState = 1;
+            this.result.Text = "0";
         }
 
         private void btnDel_Clicked(object sender, EventArgs e)
         {
-            if(lblValue.Text != "0") { 
-                lblValue.Text = lblValue.Text.Remove(lblValue.Text.Length-1);
-                if(string.IsNullOrEmpty(lblValue.Text) ) {
-                    lblValue.Text = "0";
+            if(result.Text != "0") { 
+                result.Text = result.Text.Remove(result.Text.Length-1);
+                if(string.IsNullOrEmpty(result.Text) ) {
+                    result.Text = "0";
                         }
             }
         }
 
         private void btnDot_Clicked(object sender, EventArgs e)
         {
-            if (lblValue.Text.Contains(".") is false)
+            if (result.Text.Contains(".") is false)
             {
-                lblValue.Text += ".";
+                result.Text += ".";
             }
         }
 
         private void btnGetResult_Clicked(object sender, EventArgs e)
         {
-            string final = lblHistory.Text + " " + lblValue.Text;
-            string[] arr = final.Split(" ");
-            float result = 0;
-            float tempR = 0;
-            string tempS = string.Empty;
-            for(int i=0; i<arr.Length; i++)
+           if(currentState == 2)
             {
-                if (i % 2 == 0)
-                {
-                    tempR = float.TryParse(arr[i], out float res) ? res : 0;
-                    if (result == 0)
-                    {
-                        result = tempR;
-                    }
-                    if(string.IsNullOrEmpty(tempS) is false) {
-                        switch(tempS)
-                        {
-                            case "/":
-                                result /= tempR;
-                                break;
-                            case "*":
-                                result *= tempR;
-                                break;
-                            case "+":
-                                result += tempR;
-                                break;
-                            case "-":
-                                result -= tempR;
-                                break;
-                        }
-                    }
-                    tempS = string.Empty;
-                }
-                else
-                {
-                    tempS = arr[i];
-                    lblValue.Text = result.ToString();
-                    lblHistory.Text = "0";
-                }
+                var result = Calculate.DoCalculate(firstNum, secondNum, operation);
+                this.result.Text = result.ToString();
+                firstNum = result;
+                currentState = -1;
             }
         }
         private void btnNumber_Clicked(object sender, EventArgs e)
         {
             Button b = (Button)sender;
-            float f = float.TryParse(b.Text, out float res)? res: 0;
-            lblValue.Text = lblValue.Text == "0" ? f.ToString() : lblValue.Text + f.ToString();
+            if(this.result.Text == "0" || currentState < 0)
+            {
+                this.result.Text = string.Empty;
+                if(currentState < 0)
+                {
+                    currentState *= -1;
+                }
+            }
+
+            this.result.Text += b.Text;
+
+            double number;
+            if (double.TryParse(this.result.Text, out number))
+            {
+                this.result.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNum = number;
+                }
+                else
+                {
+                    secondNum = number;
+                }
+            }
         }
     }
       
